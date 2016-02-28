288d7c144ce2e478e991a37216059d8ab324dd39
Darlan Damasio <damasio34@gmail.com>
2016-02-28 16:43:58 -0300

    Ajuste na extensão da entidade base

---
+++ Extensions/EntidadeBaseExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Damasio34.Seedwork.Extensions
+{
+    public static class EntidadeBaseExtension
+    {
+        public static bool IsNull<IEntidadeBase>(this IEntidadeBase entidade)
+        {
+            return entidade == null;
+        }
+    }
+}
