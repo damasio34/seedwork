8ab4a6475350ad36fbe36b6a1d4f8d30fb67a12f
Darlan Damasio <damasio34@gmail.com>
2016-03-02 19:45:24 -0300

    Inclusão de um novo extension.

---
+++ Exceptions/InvalidNavigationPropertyException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Damasio34.Seedwork.Exceptions
+{
+    public class InvalidNavigationPropertyException : Exception
+    {
+        public readonly string InvalidNavigationProperty;
+        public readonly Type Type;
+
+        public InvalidNavigationPropertyException(string navigationPropertyName, Type type) : base(string.Format("Navigation Property não encontrado: {0}.{1}", type.Name, navigationPropertyName))
+        {
+            if (navigationPropertyName == null) throw new ArgumentNullException("navigationPropertyName");
+            if (type == null) throw new ArgumentNullException("type");
+
+            this.InvalidNavigationProperty = navigationPropertyName;
+            this.Type = type;
+        }
+    }
+}
---
+++ Extensions/ExceptionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Damasio34.Seedwork.Extensions
+{
+    public static class ExceptionExtensions
+    {
+
+        /// <summary>
+        ///     Pega o último nível de uma Exception através da propridade <c>InnerException</c>.
+        /// </summary>
+        public static Exception GetInnermostException(this Exception ex)
+        {
+            var innermostException = ex;
+            if (innermostException == null) return null;
+
+            var ttl = 0;
+
+            while (innermostException.InnerException != null && ttl < 1000)
+            {
+                innermostException = innermostException.InnerException;
+                ttl++;
+            }
+
+            return innermostException;
+        }
+
+    }
+}
