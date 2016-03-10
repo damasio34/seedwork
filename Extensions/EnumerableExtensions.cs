61f940d94199bc94c7538df1b543f89fe0af5d4b
Darlan Damasio <damasio34@gmail.com>
2016-03-09 23:48:34 -0300

    Inclusão do comparewith

---
+++ Extensions/EnumerableExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Damasio34.Seedwork.Extensions
+{
+
+    public static class EnumerableExtensions
+    {
+        #region ' CompareWith '
+
+        public static void CompareWith<TPara, TDe>(
+            this IEnumerable<TPara> para,
+            IEnumerable<TDe> de,
+            Func<TDe, TPara, bool> comparer,
+            Action<IList<TPara>, IList<Tuple<TPara, TDe>>, IList<TDe>> callback)
+            where TDe : class
+        {
+            if (para == null) throw new ArgumentNullException("para");
+            if (de == null) throw new ArgumentNullException("de");
+            if (comparer == null) throw new ArgumentNullException("comparer");
+            if (callback == null) throw new ArgumentNullException("callback");
+
+            // identifica itens novos
+            IEnumerable<TDe> novos = de.Where(deItem => !para.Any(paraItem => comparer(deItem, paraItem)));
+
+            // identifica os itens excluidos
+            IEnumerable<TPara> excluidos = para.Where(paraItem => !de.Any(deItem => comparer(deItem, paraItem)));
+
+            // identifica itens alterados
+            IEnumerable<Tuple<TPara, TDe>> alterados = (from paraItem in para
+                let deItem = de.FirstOrDefault(deItem => comparer(deItem, paraItem))
+                where deItem != null
+                select new Tuple<TPara, TDe>(paraItem, deItem)
+                );
+
+            // executa callback
+            callback(excluidos.ToList(), alterados.ToList(), novos.ToList());
+        }
+
+        #endregion
+    }
+
+}
