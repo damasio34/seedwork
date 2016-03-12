4b6de5b5becec8fd37aa5b73dd7a096cf1e69e75
Darlan Damasio <damasio34@gmail.com>
2016-03-12 15:32:46 -0300

    Ajuste na IEntidadeDoGrupo

--- Domain/IEntidadeDoGrupo.cs
+++ Domain/IEntidadeDoGrupo.cs
@@ -2,7 +2,7 @@
 
 namespace Damasio34.Seedwork.Domain
 {
-    public interface IEntidadeDoGrupo
+    public interface IEntidadeDoGrupo : IEntidadeBase
     {
         Guid IdGrupo { get; }        
     }
